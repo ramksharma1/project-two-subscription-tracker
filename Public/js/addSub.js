2b4771671db327d2ea6cba9859e977848afbd95c
John Climie <johnclimie00@gmail.com>
2022-02-16 12:07:05 -0500

    Added addSub.js fnctionality

--- Public/js/addSub.js
+++ Public/js/addSub.js
@@ -1,19 +1,28 @@
 async function addSubscriptionFormHandler(event) {
     event.preventDefault();
 
-    const name = document.querySelector('#subName1');
-    const description = document.querySelector('#description1');
-    const link = document.querySelector('#link1');
-    const date = document.querySelector('#initDate1');
-    const cycle = document.querySelector('#cycle1');
-    const payment = document.querySelector('#payment1');
-    const remind = document.querySelector('#remind1');
+    const name = document.querySelector('#subName1').value.trim();
+    const description = document.querySelector('#description1').value.trim();
+    const link = document.querySelector('#link1').value.trim();
+    const date = document.querySelector('#initDate1').value.trim();
+    const cycle = document.querySelector('#cycle1').value.trim();
+    const payment = document.querySelector('#payment1').value.trim();
+    const remind = document.querySelector('#remind1').value.trim();
 
 
-    if ("") {
+
+    if (name, description, link, date, cycle, payment, remind) {
+
         const response = await fetch ('/api/subscriptions/', {
             method: 'POST',
             body: JSON.stringify({
+                name,
+                description,
+                link,
+                date,
+                cycle,
+                payment,
+                remind
             }),
             headers: {
                 'Content-Type': 'application/json'
@@ -22,9 +31,11 @@ async function addSubscriptionFormHandler(event) {
         if (response.ok) {
             console.log('success');
 
-            document.location.replace('/dashboard');
+            // document.location.replace('/dashboard');
         } else {
             alert(response.statusText);
         }
     }
 }
+
+document.querySelector('#addSub').addEventListener('submit', addSubscriptionFormHandler);
